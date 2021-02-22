391988641d28e2fc00824a21aac10bcfcd762ca9
Yali Wang <ywang35@alaska.edu>
2021-02-21 18:16:39 -0900

    Lambda callBack.

--- Lambda/Lambda.cpp
+++ Lambda/Lambda.cpp
@@ -3,8 +3,16 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <functional>
 
 using namespace std;
+
+std::function<void()> Fun;
+
+void callBack(std::function<void()> fun) {
+	cout << "This is a callback. \n";
+	fun();
+}
 
 int Foo(int a, int b) {
 	return a + b;
@@ -47,11 +55,17 @@ int main() {
 	//cout << c << endl;
 
 	// Lambda executed as a thread parameter
-	string s = "test";
-	thread f = thread([&s](int a, int b) {
-		cout << s << endl;
-		cout << a + b << endl;
-	}, 2, 3);
-	f.join();
+	//string s = "test";
+	//thread f = thread([&s](int a, int b) {
+	//	cout << s << endl;
+	//	cout << a + b << endl;
+	//}, 2, 3);
+	//f.join();
+	
+	// Lambda callBack
+	callBack([]() {
+		cout << "This is a lambda. \n" <<endl;
+	});
+	system("pause");
 }
 
