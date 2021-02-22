8ae60ef02428c702c6c0e78508add3888c316776
Yali Wang <ywang35@alaska.edu>
2021-02-21 17:38:38 -0900

    Completed function nesting

--- Lambda/Lambda.cpp
+++ Lambda/Lambda.cpp
@@ -13,15 +13,24 @@ int main() {
 	// Lambda functions are similar to anonymous functions
 	
 	// Simple example
-	int c = [](int a, int b) -> int { // int on the latter part is the returning type
-	return a + b;
+	//int c = [](int a, int b) -> int { // int on the latter part is the returning type
+	//return a + b;
 
-	}(1, 2);
+	//}(1, 2);
 
-	auto f = [](int a, int b) -> int { // int on the latter part is the returning type
-		return a + b;
-	};
-	c = f(1, 2);
+	//auto f = [](int a, int b) -> int { // int on the latter part is the returning type
+	//	return a + b;
+	//};
+	//c = f(1, 2);
+	//cout << c << endl;
+
+	// Function nested in another function
+	// Could be used in multithreading and parallel
+	int c = [](int m) {
+		return [m](int n) {
+		return m + n;
+		}(1);
+	}(2);
 	cout << c << endl;
 
 
